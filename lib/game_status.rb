1b67860556cf077be8dc79d554f65beba6c03da5
Sonata Arrington <sarrington1009@gmail.com>
2019-10-06 17:14:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,27 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+  
+  def won?(board)
+    counter = 0
+      WIN_COMBINATIONS. each |win_combination|
+      puts 
+    #   counter += 1 
+    #   if counter >= 8
+    #     break
+    #   end
+    # end
+    # return false
+  end 
+  
+  position1 = WIN_COMBINATIONS
